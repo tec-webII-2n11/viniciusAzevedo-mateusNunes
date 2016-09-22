b5ab45f00091123a4fb31216ace98969dfa301df
Vinicius Azevedo de Lima <azvini@outlook.com>
2016-09-22 05:11:55 +0000

    Atualização do wireframe

---
+++ menu.php
@@ -0,0 +1,10 @@
+
+        <ul id="menu-bar">
+             <li class="active"><a href="#">Home</a></li>
+             <li><a href="#">Feed de Publicações</a>
+         </li>
+         <li><a href="#">Suporte</a>
+         </li>
+         <li><a href="#">Sobre o site</a></li>
+         <li><a href="#">Saída</a></li>
+        </ul>
---
+++ site.php
@@ -0,0 +1,15 @@
+<!doctype HTML>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <meta name = "author" content ="Vinícius Azevedo - 31603289 / Mateus Nunes - 31657461">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <header>
+        <?php
+            include 'menu.php';
+        ?>
+        </header>
+    </body>
+</html>
